df73f75bd3e3947e43ed7c031a994a60ae050f2c
Dganzh <dganzh@163.com>
2018-02-18 20:53:17 +0800

    pymysql as mysqldb

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -1,4 +1,6 @@
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
