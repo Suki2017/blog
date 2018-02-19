9808d460172132c13e70371d07e95f153b033188
root <root@www.dganzh.cc>
2018-02-19 02:25:03 -0500

    upload

--- myBlog/wsgi.py
+++ myBlog/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myBlog.settings")
 
 application = get_wsgi_application()
+
+#def application(env, start_response):
+#    start_response('200 OK', [('Content-Type', 'text/html')])
+#    return b'my blog'
+
+
+
