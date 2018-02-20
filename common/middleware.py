1c36ef26b64cd6bf187ee8a777e74c24d04dee78
root <root@www.dganzh.cc>
2018-02-20 07:50:53 -0500

    fix middleware log path

--- common/middleware.py
+++ common/middleware.py
@@ -1,6 +1,8 @@
 import logging
 import datetime
+import os
 
+base_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 class LoggingExceptionMiddleware(object):
     def __init__(self, get_response):
@@ -9,7 +11,7 @@ class LoggingExceptionMiddleware(object):
     def process_exception(self, request, exception):
         logger = logging.getLogger('ws_logger')
         logger.setLevel(logging.ERROR)
-        logname = 'ws_logging/%s.log' % str(datetime.datetime.now().date())
+        logname = os.path.join(base_path, 'ws_logging/%s.log' % str(datetime.datetime.now().date()))
         filehandler = logging.FileHandler(filename=logname, encoding="utf-8")
         fmter = logging.Formatter(fmt="[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
                                   datefmt="%Y-%m-%d %H:%M:%S")
